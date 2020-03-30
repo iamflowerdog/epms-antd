5225ac7024a93ae63f4ce2a0433b239f1dcdf04a
Mr.YanG <yangyonghai9125@hotmail.com>
2020-03-30 18:11:32 +0800

    添加listpage页面

---
+++ src/view/list-page/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import axios from "axios";
+import { bindActionCreators, createStore, applyMiddleware } from "redux";
+import { Provider, connect } from "react-redux";
+import thunk from "redux-thunk";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Pagination, Table, Input } from "antd";
+import ListPage from "./ListPage";
+import DetailPage from "./DetailPage";
+import reducer from "./reducer";
+
+import "./index.less";
+const createLogger = require("redux-logger").createLogger;
+const logger = createLogger({ collapsed: true });
+
+// Create store
+const store = createStore(reducer, {}, applyMiddleware(thunk, logger));
+
+export default class ListPageSample extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="list-page-app">
+            <Route path="/list-page/:page?" component={ListPage} />
+            <Route path="/user/:userId" component={DetailPage} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ListPageSample/>,
+  document.getElementById('root')  
+)
